d194baba3a409420c38d48fd3202fe1ed7bd0069
MariaCsantosbela <104848675+MariaCsantosbela@users.noreply.github.com>
2022-07-18 18:57:03 -0300

    Add files via upload

---
+++ main.mjs
@@ -0,0 +1,6 @@
+import {  mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+console.log(mostraIdade("Maria", 43));
+console.log(mostraCidade("Maria","Recife"));
+console.log(mostraHobby("Maria","musica"));
+
+
